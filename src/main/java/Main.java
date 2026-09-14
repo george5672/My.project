c3f99adbb80bd14864d8437313426888c83c9cb2
Sanya550 <pivovarsasa9@gmail.com>
2026-09-14 19:12:35 +0300

    Add ArrayUtils with merge sort and binary search

---
+++ src/main/java/Main.java
@@ -0,0 +1,74 @@
+import java.util.Random;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+
+        System.out.println("Початковий масив: " + Arrays.toString(array));
+
+        mergeSort(array);
+        System.out.println("Відсортований масив: " + Arrays.toString(array));
+
+        int target = array[4];
+        int index = binarySearch(array, target);
+        System.out.println("Значення " + target + " знайдено на індексі: " + index);
+    }
+
+    public static void mergeSort(int[] array) {
+        if (array == null || array.length < 2) {
+            return;
+        }
+        int mid = array.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[array.length - mid];
+
+        System.arraycopy(array, 0, left, 0, mid);
+        System.arraycopy(array, mid, right, 0, array.length - mid);
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(array, left, right);
+    }
+
+    private static void merge(int[] array, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                array[k++] = left[i++];
+            } else {
+                array[k++] = right[j++];
+            }
+        }
+        while (i < left.length) {
+            array[k++] = left[i++];
+        }
+        while (j < right.length) {
+            array[k++] = right[j++];
+        }
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] == target) {
+                return mid;
+            }
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
